e5e698182e5768b00c880a5fa50518204fa048ce
Fadil A <38697321+fbdel@users.noreply.github.com>
2019-03-13 21:53:28 -0400

    Add files via upload

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -4,12 +4,37 @@ module.exports = function(app) {
   // Load index page
   app.get("/", function(req, res) {
     db.Example.findAll({}).then(function(dbExamples) {
-      res.render("index", {
+      res.render("login-register", {
         msg: "Welcome!",
         examples: dbExamples
       });
     });
   });
+
+
+  // Load Survey page
+  app.get("/survey", function(req, res) {
+    db.Example.findAll({}).then(function(dbExamples) {
+      res.render("survey-page", {
+        msg: "Welcome!",
+        examples: dbExamples
+      });
+    });
+  });
+
+
+  // Load Survey page
+  app.get("/charades", function(req, res) {
+    db.Example.findAll({}).then(function(dbExamples) {
+      res.render("charades", {
+        msg: "Welcome!",
+        examples: dbExamples
+      });
+    });
+  });
+
+
+ 
 
   // Load example page and pass in an example by id
   app.get("/example/:id", function(req, res) {
